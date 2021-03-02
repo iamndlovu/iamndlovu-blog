1b4fea7a4b851a9eb0fb9fcaef6b6874f7fd02b1
iamndlovu <ndlovu.p@protonmail.com>
2021-03-02 22:27:22 +0200

    edit app meta tags and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Helmet } from 'react-helmet';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      BLOG
+      <Helmet>
+          <title>Blog - Pardon Ndlovu | Front-End Developer</title>
+          <meta name="description" content="Pardon Ndlovu's blog documenting web development journey" />
+          <meta name="theme-color" content="#008f68" />
+        </Helmet>
+      <Layout>
+        <h1>Blog</h1>
+      </Layout>
     </div>
   );
 }
