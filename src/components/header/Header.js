2e3fd3e3bcaa4f82f6cca832f44a6992f289bbe6
iamndlovu <ndlovu.p@protonmail.com>
2021-03-02 22:29:54 +0200

    shared components: footer & header

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='Header'>
+      <h1>Pardon Ndlovu's Blog</h1>
+      <h2>Welcome to my blog</h2>
+    </header>
+  )
+}
+
+export default Header
