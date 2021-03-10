479932f1ada7e98966f5e72ce4126ad18ec778ce
iamndlovu <ndlovu.p@protonmail.com>
2021-03-11 01:37:57 +0200

    add side padding and border-top

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+
+import footerStyles from "./Footer.module.scss";
 
 const Footer = () => {
-  return (
-    <footer className="Footer">
-      &copy; { new Date().getFullYear() } Pardon Ndlovu
-    </footer>
-  )
-}
+	return (
+		<footer className={footerStyles.Footer}>
+			&copy; {new Date().getFullYear()} Pardon Ndlovu
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
